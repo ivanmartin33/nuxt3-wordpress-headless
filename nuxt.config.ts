fd3f6fa7055562a342a225f33c41b859b639c4ee
ivanmartin33 <ivan@ivanmartin.fr>
2022-06-03 10:10:57 +0200

    Config Nuxt

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,30 @@
-import { defineNuxtConfig } from 'nuxt'
+import { defineNuxtConfig } from "nuxt";
 
-// https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
+	css: ["@/assets/css/tailwind.css"],
+	modules: ["nuxt-graphql-client"],
+	autoImports: { global: true },
+	runtimeConfig: {
+		public: {
+			GQL_HOST: process.env.GQL_HOST,
+			onlyOperationTypes: true,
 
-})
+			"graphql-client": {
+				clients: {
+					default: process.env.GQL_HOST,
+				},
+			},
+		},
+	},
+
+	build: {
+		postcss: {
+			postcssOptions: {
+				plugins: {
+					tailwindcss: {},
+					autoprefixer: {},
+				},
+			},
+		},
+	},
+});
