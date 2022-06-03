a25f8ef3bd57f2158ce1d1912320dd5a52457af9
ivanmartin33 <ivan@ivanmartin.fr>
2022-06-03 10:11:49 +0200

    Config tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,30 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
+module.exports = {
+	mode: "jit",
+	content: [
+		`components/**/*.{vue,js}`,
+		`layouts/**/*.vue`,
+		`pages/**/*.vue`,
+		`composables/**/*.{js,ts}`,
+		`plugins/**/*.{js,ts}`,
+		`App.{js,ts,vue}`,
+		`app.{js,ts,vue}`,
+	],
+	theme: {
+		extend: {
+			fontFamily: {
+				sans: ['"Plus Jakarta Sans"', ...defaultTheme.fontFamily.sans],
+			},
+		},
+	},
+	variants: {
+		extend: {},
+	},
+	plugins: [
+		require("@tailwindcss/forms"),
+		require("@tailwindcss/typography"),
+		require("@tailwindcss/line-clamp"),
+		require("@tailwindcss/aspect-ratio"),
+	],
+};
