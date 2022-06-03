e6ebd0d0783a7d5882872beeb67bc792a74d828d
ivanmartin33 <ivan@ivanmartin.fr>
2022-06-03 10:14:26 +0200

    Add auth types

---
+++ types/auth.ts
@@ -0,0 +1,33 @@
+export interface User {
+	databaseId?: number;
+	name?: string;
+	email?: string;
+	firstName?: string;
+	lastName?: string;
+	jwtAuthToken?: string;
+	jwtRefreshToken?: string;
+	jwtAuthExpiration?: string;
+}
+
+export const DEFAULT_USER: User = {
+	databaseId: 0,
+	name: "",
+	firstName: "",
+	lastName: "",
+	email: "",
+	jwtAuthExpiration: "",
+	jwtAuthToken: "",
+	jwtRefreshToken: "",
+};
+
+export interface AuthData {
+	loggedIn: boolean;
+	user?: User;
+	error?: Error;
+}
+
+export const DEFAULT_STATE: AuthData = {
+	loggedIn: false,
+	user: undefined,
+	error: undefined,
+};
